07309f9fc78524bbc358a86e40b51a3fd819656b
greatlaki <greatlaki911@gmail.com>
2021-10-15 23:55:43 +0300

    Creating another view function: displaying a list of articles by category

--- enjoyinglearn/enjoying_people/views.py
+++ enjoyinglearn/enjoying_people/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('Страница приложения enjoying_people.')
+
+
+def categories(request):
+    return HttpResponse("<h1>Статьи по категориям</h1>")
--- enjoyinglearn/enjoyinglearn/urls.py
+++ enjoyinglearn/enjoyinglearn/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from enjoying_people.views import index
+from enjoying_people.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('people/', index),
+    path('cats/', categories)
 ]
+
