50077402502f9c3afca68a8265407d444133fae5
greatlaki <greatlaki911@gmail.com>
2021-10-18 18:49:14 +0300

    Making changes to the writing of the code

--- enjoyinglearn/enjoyinglearn/urls.py
+++ enjoyinglearn/enjoyinglearn/urls.py
@@ -14,14 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from video_content.views import upload_video,display
-
-from django.conf.urls.static import static
-from django.conf import  settings
 
 
+from django.conf import settings
 from enjoying_people.views import *
 from django.urls import path, include
 
@@ -32,8 +30,9 @@ urlpatterns = [
     path('videos/', display, name='videos'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 handler404 = pageNotFound
