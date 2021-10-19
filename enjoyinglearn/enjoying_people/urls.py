86ef82e803cf27effa93314c97089848a332f2d4
greatlaki <greatlaki911@gmail.com>
2021-10-19 18:49:54 +0300

    I added three more routes to the urlpatterns collection

--- enjoyinglearn/enjoying_people/urls.py
+++ enjoyinglearn/enjoying_people/urls.py
@@ -5,4 +5,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
+    path('add_page/', add_page, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
 ]
--- enjoyinglearn/enjoying_people/views.py
+++ enjoyinglearn/enjoying_people/views.py
@@ -19,13 +19,6 @@ def about(request):
     return render(request, 'enjoying_people/about.html')
 
 
-def categories(request, catid):
-    if request.POST:
-        print(request.POST)
-
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
-
-
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
