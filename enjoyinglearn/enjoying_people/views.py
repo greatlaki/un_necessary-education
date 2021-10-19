6c9b646565b87ebd0d69358c6be77f1976b8c441
greatlaki <greatlaki911@gmail.com>
2021-10-19 18:44:50 +0300

    Changing the menu item, I defined it as a list of dictionaries

--- enjoyinglearn/enjoying_people/views.py
+++ enjoyinglearn/enjoying_people/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render, redirect
 
 from .models import *
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{"title": "О сайте", "url_name": 'about'},
+        {"title": "Добавить статью", "url_name": 'add_page'},
+        {"title": "Обратная связь", "url_name": 'contact'},
+        {"title": "Войти", "url_name": 'login'},
+]
 
 
 def index(request):
