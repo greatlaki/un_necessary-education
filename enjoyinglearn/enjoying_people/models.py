e1498d2bf0bd8aae25ef70b00363e5f9f52dec47
greatlaki <greatlaki911@gmail.com>
2021-10-20 14:12:55 +0300

    Creating relationships between models via the ForeignKey class

--- enjoyinglearn/enjoying_people/models.py
+++ enjoyinglearn/enjoying_people/models.py
@@ -13,7 +13,7 @@ class People(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
+    def get_absolute_url(self): # Отображение элемента базы данных
         return reverse('post', kwargs={'post_id': self.pk})
 
 
