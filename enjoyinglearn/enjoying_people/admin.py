16a3f3acc51473cf1c1f7f6cd4764652bcb34b4f
greatlaki <greatlaki911@gmail.com>
2021-10-20 19:30:41 +0300

    Fixes incorrectly added attributes. I registered a name for a simple tag, using the "name" parameter

--- enjoyinglearn/enjoying_people/admin.py
+++ enjoyinglearn/enjoying_people/admin.py
@@ -7,14 +7,15 @@ class PeopleAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create')
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
-    list_editable = ('is_published',)
-    list_filter = ('is_published', 'time_create')
+
 
 
 admin.site.register(People, PeopleAdmin)
