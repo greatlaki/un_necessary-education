272fe4479c567fb1d14727f2c050e42d200e6970
greatlaki <greatlaki911@gmail.com>
2021-10-20 23:36:21 +0300

    The reverse_lazy() function, which is different from reverse()

--- enjoyinglearn/enjoying_people/views.py
+++ enjoyinglearn/enjoying_people/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 
 
@@ -36,6 +37,7 @@ def about(request):
 class AddPage(CreateView):
     form_class = AddPostForm
     template_name = 'enjoying_people/addpage.html'
+    success_url = reverse_lazy('home')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
