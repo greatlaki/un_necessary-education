f8462afd0b49fb7662397515cba95f618916f6cc
greatlaki <greatlaki911@gmail.com>
2021-10-20 21:56:13 +0300

    Assigning an individual field style, using the widgets attribute

--- enjoyinglearn/enjoying_people/forms.py
+++ enjoyinglearn/enjoying_people/forms.py
@@ -5,4 +5,8 @@ from .models import *
 class AddPostForm(forms.ModelForm):
     class Meta:
         model = People
-        fields = '__all__'
+        fields = ['title', 'slug', 'content', 'is_published', 'cat']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols':60, 'rows':10}),
+        }
