1173d011d39cb930e55607eb8103149f47cd5771
greatlaki <greatlaki911@gmail.com>
2021-10-20 18:21:07 +0300

    Configuring the admin panel. I have prescribed an auxiliary class "Administrator of people", the attributes of which contain a list of fields that will be displayed, fields that can be clicked and by which you can search for information

--- enjoyinglearn/enjoying_people/admin.py
+++ enjoyinglearn/enjoying_people/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import *
 
+class PeopleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
+
 admin.site.register(People)
