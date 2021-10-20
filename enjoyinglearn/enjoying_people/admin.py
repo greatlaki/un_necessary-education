49b1d86cc42896c410b4ad3c6832a3bd6b4ef02e
greatlaki <greatlaki911@gmail.com>
2021-10-20 18:27:06 +0300

    Registration of the Category model and the auxiliary class CategoryAdmin

--- enjoyinglearn/enjoying_people/admin.py
+++ enjoyinglearn/enjoying_people/admin.py
@@ -9,4 +9,11 @@ class PeopleAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
 admin.site.register(People, PeopleAdmin)
+admin.site.register(Category, CategoryAdmin)
