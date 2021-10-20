43ad1eed24611d8f630ef167756962cfa4b43e39
greatlaki <greatlaki911@gmail.com>
2021-10-20 19:35:21 +0300

    Definition of the enclosing tag that will return a fragment of the html page. Automatically enters the dictionary into the template "enjoying_people/list_categories.html"

--- enjoyinglearn/enjoying_people/templatetags/enjoying_people_tags.py
+++ enjoyinglearn/enjoying_people/templatetags/enjoying_people_tags.py
@@ -7,3 +7,9 @@ register = template.Library()
 @register.simple_tag(name='getcats')
 def get_categories():
     return Category.objects.all()
+
+
+@register.inclusion_tag('enjoying_people/list_categories.html')
+def show_categories():
+    cats = Category.objects.all()
+    return {"cats": cats}
