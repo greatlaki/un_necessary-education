333c0f9bffd3d85608f179d9bfa35293cca59ab5
greatlaki <greatlaki911@gmail.com>
2021-10-20 21:23:59 +0300

    Added a new attribute - slug, to the People model

--- enjoyinglearn/enjoying_people/models.py
+++ enjoyinglearn/enjoying_people/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 class People(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(blank=True, verbose_name='Текст статьи')
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name='Фото')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
