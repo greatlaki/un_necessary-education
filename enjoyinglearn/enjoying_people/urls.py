5eb9deb51235fb8572c0b33bd491e5f2d96ebf40
greatlaki <greatlaki911@gmail.com>
2021-10-20 15:49:03 +0300

    Made some changes to make everything work as it should

--- enjoyinglearn/enjoying_people/urls.py
+++ enjoyinglearn/enjoying_people/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
     path('post/<int:post_id>/', show_post, name='post'),
-    path('category/<int:cad_id>/', show_category, name='category'),
+    path('category/<int:cat_id>/', show_category, name='category'),
 ]
