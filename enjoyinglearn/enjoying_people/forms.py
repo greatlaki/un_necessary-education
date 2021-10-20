9368dba0d73d4bc95d41e4c958a3737488bf8656
greatlaki <greatlaki911@gmail.com>
2021-10-20 22:00:03 +0300

    Called the constructor, set the empty_label attribute, now instead of "------" -> " Category not selected"

--- enjoyinglearn/enjoying_people/forms.py
+++ enjoyinglearn/enjoying_people/forms.py
@@ -3,6 +3,10 @@ from .models import *
 
 
 class AddPostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label= 'Категория не выбрана'
+
     class Meta:
         model = People
         fields = ['title', 'slug', 'content', 'is_published', 'cat']
