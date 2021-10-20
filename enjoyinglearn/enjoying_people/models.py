2e3f4cdd768dfb6ec629522108b27f85259e12e3
greatlaki <greatlaki911@gmail.com>
2021-10-20 18:29:58 +0300

    Now it is easier to work with categories in the admin panel

--- enjoyinglearn/enjoying_people/models.py
+++ enjoyinglearn/enjoying_people/models.py
@@ -24,10 +24,15 @@ class People(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_plural = 'Категории'
+        ordering = ['id']
