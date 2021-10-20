498c51c15ab257bdc7e55ddcd2b4ba00e225220b
greatlaki <greatlaki911@gmail.com>
2021-10-20 21:31:49 +0300

    I'm reconfiguring the database

--- enjoyinglearn/enjoying_people/models.py
+++ enjoyinglearn/enjoying_people/models.py
@@ -10,7 +10,7 @@ class People(models.Model):
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категории')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категории')
 
     def __str__(self):
         return self.title
@@ -26,6 +26,7 @@ class People(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
     def __str__(self):
         return self.name
