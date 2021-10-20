1388cb4f7b86c1cd207dc23801e218d877a7f38a
greatlaki <greatlaki911@gmail.com>
2021-10-20 18:23:59 +0300

    Registered the People Admin class and assigned alternative names to the attributes of the People model

--- enjoyinglearn/enjoying_people/admin.py
+++ enjoyinglearn/enjoying_people/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 from .models import *
 
+
 class PeopleAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
 
 
-admin.site.register(People)
+admin.site.register(People, PeopleAdmin)
