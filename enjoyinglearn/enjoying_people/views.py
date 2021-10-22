fbb3342639a1d338634563281f76ad15db517dd8
greatlaki <greatlaki911@gmail.com>
2021-10-22 13:32:36 +0300

    Now I have formed a common context based on two dictionaries

--- enjoyinglearn/enjoying_people/views.py
+++ enjoyinglearn/enjoying_people/views.py
@@ -16,7 +16,7 @@ class PeopleHome(DataMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title='Главная страница')
-        return context
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return People.objects.filter(is_published=True)
