b9f63454ba9c0a740eb5bb597f5cf2faa58dab35
greatlaki <greatlaki911@gmail.com>
2021-10-22 19:10:51 +0300

    Added a link to register new users

--- enjoyinglearn/enjoying_people/urls.py
+++ enjoyinglearn/enjoying_people/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('addpage/', AddPage.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
+    path('register/', login, name=register),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
     path('category/<slug:cat_slug>/', PeopleCategory.as_view(), name='category'),
 ]
