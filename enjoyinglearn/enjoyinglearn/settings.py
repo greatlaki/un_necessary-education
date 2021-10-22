12516a7335fbe9342e905d5253895a54dca039ac
greatlaki <greatlaki911@gmail.com>
2021-10-22 20:46:52 +0300

    I am doing further optimization of the site using the cache, namely: caching to the file system

--- enjoyinglearn/enjoyinglearn/settings.py
+++ enjoyinglearn/enjoyinglearn/settings.py
@@ -141,3 +141,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_cache',
+    }
+}
