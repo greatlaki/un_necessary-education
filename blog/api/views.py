59b9744155dbf6e828094deabaa71dc2c79c128d
greatlaki <greatlaki911@gmail.com>
2021-10-28 00:50:16 +0300

    Submission of comments. Added "CommentList" and "commentdetail"

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,10 +1,10 @@
 from rest_framework import generics, permissions
 from django.contrib.auth.models import User
-from .models import *
-
-from . import serializers
 from .permisisions import IsOwnerOrReadOnly
 from .serializers import PostSerializer
+
+from .models import *
+from . import serializers
 
 
 class UserList(generics.ListAPIView):
@@ -29,5 +29,21 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly]
