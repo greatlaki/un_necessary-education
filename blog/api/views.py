4a75cdf9fd0bb93273531c9231d0021a40c0e59b
greatlaki <greatlaki911@gmail.com>
2021-10-28 00:35:09 +0300

    Added "IsAuthenticatedOrReadOnly". With it, any authenticated user can make any request, and the rest are read—only.

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 from .models import *
 
 from . import serializers
+from .permisisions import IsOwnerOrReadOnly
+from .serializers import PostSerializer
 
 
 class UserList(generics.ListAPIView):
@@ -17,7 +19,8 @@ class UserDetail(generics.RetrieveAPIView):
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
-    serializer_class = serializers.PostSerializer
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -25,4 +28,6 @@ class PostList(generics.ListCreateAPIView):
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
-    serializer_class = serializers.PostSerializer
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
